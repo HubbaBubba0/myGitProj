4ce133ff16b1a88348dc3cf857fc5f2546acb6ce
GaSapi <hubba_bubba0@yahoo.com>
2015-12-22 16:13:53 +0200

    SugarCrm login page

---
+++ yissik/myGitProj/src/main/java/com/SugarCrm/PODdriver/LoginPage.java
@@ -0,0 +1,42 @@
+package com.SugarCrm.PODdriver;
+
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.support.FindBy;
+        import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Created by ninja on 22/12/15.
+ */
+public class LoginPage {
+    @FindBy(name = "user_name")
+    private WebElement userNameBox;
+    @FindBy(name = "user_password")
+    private WebElement passwordBox;
+    private WebDriver driver;
+
+    LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void fillinNameBox(String userName) {
+        userNameBox.sendKeys(userName);
+    }
+
+    public void fillinPassword(String userPassword) {
+        passwordBox.sendKeys(userPassword);
+    }
+
+    public void loginSubmit() {
+        passwordBox.submit();
+    }
+
+    public HomePage fillinAndSubmit(String userName, String userPassword) {
+        fillinNameBox(userName);
+        fillinPassword(userPassword);
+        loginSubmit();
+
+        return new HomePage(driver);
+    }
+}
---
+++ yissik/myGitProj/src/main/java/com/SugarCrm/PODdriver/PODChromeDriver.java
@@ -0,0 +1,79 @@
+package com.SugarCrm.PODdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class PODChromeDriver {
+    public static void main(String[] args) {
+
+        String userName = "admin";
+        String userPassword = "admin";
+
+        // Create a new instance of the Chrome driver and a webDriverWait
+//        SugarCrmBasePage sugarCrm = new SugarCrmBasePage();
+        System.setProperty("webdriver.chrome.driver", "/home/ninja/Downloads/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        // Go to sugar crm
+        driver.get("http://localhost/sugar/SugarCE-Full-6.5.22/index.php?action=Login&module=Users");
+
+        //Login
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = loginPage.fillinAndSubmit(userName, userPassword);
+//
+//        //navigate to leads page
+//        LeadsPage leadsPage = homePage.navigateToLeadsPage();
+//
+//        //Create Lead
+//        CreateNewLeadPage createNewLeadPage = leadsPage.navigateToCreateNewLeadPage();
+//
+//
+////        //New Lead
+////
+//        //create unique first and last names
+//        int randomizingCounter = 0;
+//        Date date = new Date(System.currentTimeMillis());
+//        String firstName = "" + randomizingCounter++ + date.getTime();
+//        String lastName = "" + randomizingCounter++ + date.getTime();
+//        String salutation = "Prof.";
+//
+//        //fill in form
+////
+////        //first name
+//        createNewLeadPage.enterFirstName(firstName);
+//        createNewLeadPage.enterLastName(lastName);
+//        createNewLeadPage.enterSalutation(salutation);
+//        createNewLeadPage.checkCopyAddressFromLeftCheckbox();
+//        createNewLeadPage.submitNewLead();
+
+//        //go back to lead search
+//        driver.findElement(By.linkText("Leads")).click();
+//
+//        //search for the created user
+//        wdWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("search_name_basic")));
+//        WebElement search_name_basic = driver.findElement(By.id("search_name_basic"));
+//        search_name_basic.clear();
+//        search_name_basic.sendKeys(lastName);
+//        search_name_basic.submit();
+//
+//        //validate user appears in list
+//        wdWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("MassUpdate")));
+//        try {
+//            driver.findElement(By.xpath("//*[contains(text(), lastName)]"));
+//        } catch (ElementNotFoundException e){
+//            Assert.assertTrue(true);
+//        }
+//
+//        System.out.println("New user found");
+//
+//        //Close the browser
+//        driver.quit();
+//
+////        driver.get();
+    }
+}
